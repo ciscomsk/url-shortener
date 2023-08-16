6f0f72eb5b28c65e407a322389c7b79e0b1476bb
ciscomsk <kuznetzoff@gmail.com>
2023-08-16 18:11:57 +0300

    First commit

---
+++ cmd/url_shortener/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
+
+func main() {
+	// TODO: init config: cleanenv
+	cfg := config.MustLoad()
+	fmt.Print(cfg)
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi + chi render
+
+	// TODO: run server
+}
